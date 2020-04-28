5d0734d8d088b155f5ae9b285c0b040062a47a61
veuda <62720180+veuda@users.noreply.github.com>
2020-04-28 14:44:14 +0200

    mise en marche du fichier

--- js/dates_palavanne.js
+++ js/dates_palavanne.js
@@ -1,92 +1,19 @@
-var prestas = [
-    {
-        spectacle: "La Moufle",
-        date: "20 février",
-        ville: "Viriat (01)",
-        evenement: "École maternelle Les Prairies",
-        lien: ""
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "28 février",
-        ville: "Les Mathes (17)",
-        evenement: "Carnaval de la débrouille",
-        lien: "https://www.royanatlantique.fr/"
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "16 mai",
-        ville: "Nevers (25)",
-        evenement: "Sensibilisation au développement durable",
-        lien: "https://www.nevers.fr/"
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "20 mai",
-        ville: "Sevran (93)",
-        evenement: "Immobilière 3F",
-        lien: "https://www.ville-sevran.fr/"
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "2 et 3 juin",
-        ville: "Gex (01)",
-        evenement: "Festival Tôt ou tard",
-        lien: "https://www.festivaltotoutarts.com/"
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "6 et 7 juin",
-        ville: "Estavayer-le-Lac (Suisse)",
-        evenement: "Festival Ludimaniak 2020",
-        lien: "https://ludimaniak.ch/fr/"
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "13 juin",
-        ville: "Oyonnax (01)",
-        evenement: "Défilé des conscrits",
-        lien: ""
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "19 juin",
-        ville: "Arc et Senans (25)",
-        evenement: "70 ans de Dynacité",
-        lien: ""
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "19 septembre",
-        ville: "Montargis (45)",
-        evenement: "Fête du quartier sur le développement durable",
-        lien: ""
-    },
-    {
-        spectacle: "Les Pourkoapas",
-        date: "23 septembre",
-        ville: "Sevran (93)",
-        evenement: "Immobilière 3F",
-        lien: ""
+var prestas = []
+
+fetch('./json/prestas-palavanne.json')
+.then(function(response) {
+    return response.json() 
+})
+.then(function(data) { 
+    prestas = data
+
+    var table = document.querySelector('.agenda')
+
+    for (var i = 0; i < prestas.length; i++) {
+        var dates = createRow(prestas[i])
+        table.appendChild(dates)
     }
-]
-
-// var prestas = []
-
-// fetch('./json/prestas.json')
-// .then(function(response) {
-//     return response.json() 
-// })
-// .then(function(data) { 
-//     prestas = data
-// })
-
-var table = document.querySelector('.agenda')
-
-for (var i = 0; i < prestas.length; i++) {
-    var dates = createRow(prestas[i])
-    table.appendChild(dates)
-}
+})
 
 function createRow(presta) {
     var tr = document.createElement('tr')
@@ -97,6 +24,7 @@ function createRow(presta) {
 
         var lienSpectacle = document.createElement('a')
         lienSpectacle.href = 'pourkoapas.html'
+        lienSpectacle.classList = 'link-spectacle'
         lienSpectacle.innerHTML = presta.spectacle
         cell.appendChild(lienSpectacle)
 
@@ -107,6 +35,7 @@ function createRow(presta) {
 
         var lienSpectacle = document.createElement('a')
         lienSpectacle.href = 'moufle.html'
+        lienSpectacle.classList = 'link-spectacle'
         lienSpectacle.innerHTML = presta.spectacle
         cell.appendChild(lienSpectacle)
 
@@ -118,9 +47,10 @@ function createRow(presta) {
     tr.appendChild(createCell(presta.date))
     tr.appendChild(createCell(presta.ville))
 
-    if (presta.lien !== '') {
+    if (presta.lien !== '' && presta.spetacle !== '') {
         var lien = document.createElement('a')
         lien.href = presta.lien
+        lienSpectacle.classList = 'link-presta'
         lien.innerHTML = presta.evenement
         
         var td = document.createElement('td')
