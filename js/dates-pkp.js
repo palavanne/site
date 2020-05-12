5606524fe60d3027c58e9334fd6b36bf5abab800
veuda <62720180+veuda@users.noreply.github.com>
2020-05-12 21:11:49 +0200

    Récup site

---
+++ js/dates-pkp.js
@@ -0,0 +1,52 @@
+(function () {
+
+    var prestas = []
+
+    fetch('json/dates-options.json')
+    .then(function(response) { 
+        return response.json()
+    })
+    .then(function(data) { 
+        prestas = data
+        
+        let table = document.querySelector('.agenda')
+        
+        for (let i = 0; i < prestas.length; i++) {
+            if (!prestas[i].option && prestas[i].spectacle === 'Les Pourkoapas') {
+                let dates = createRow(prestas[i])
+                table.appendChild(dates)
+            }
+        }
+    })
+
+    function createRow(presta) {
+        let divDate = document.createElement('div')
+        divDate.classList.add('row', 'pourkoapas', 'white')
+        divDate.appendChild(createCell(presta.date + ' ' + presta.annee))
+        divDate.appendChild(createCell(presta.ville))
+        divDate.appendChild(createCell(presta.evenement))
+
+        if (presta.lien !== '') {
+            let lien = document.createElement('a')
+            lien.href = presta.lien
+            lien.classList = 'btn-agenda'
+            lien.innerHTML = 'Plus d\'infos'
+            
+            let divLien = document.createElement('div')
+            divLien.appendChild(lien)
+
+            divDate.appendChild(divLien)
+        }
+
+        return divDate
+    }
+
+    function createCell(val) {
+        let divCell = document.createElement('div')
+        divCell.classList.add('cell')
+        divCell.innerHTML = val
+
+        return divCell
+    }
+
+} ())
---
+++ js/modal.js
@@ -0,0 +1,13 @@
+const buttonOpen = document.querySelector('.button')
+buttonOpen.addEventListener('click', function () {
+    const modalOpen = document.querySelector('.modal')
+    modalOpen.classList.remove('hidden')
+    document.querySelector('.youtube-video').contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+})
+
+const btnClose = document.querySelector('.close-modal')
+btnClose.addEventListener('click', function () {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('hidden')
+    document.querySelector('.youtube-video').contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+})
