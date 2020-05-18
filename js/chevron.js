ec1b7026c7c609d040f1a23c968a819a6e283eff
veuda <62720180+veuda@users.noreply.github.com>
2020-05-18 18:02:42 +0200

    remove chevron and cards

--- js/chevron.js
+++
@@ -1,6 +0,0 @@
-const chevronPkp = document.querySelector('.chevron-pkp')
-
-chevronPkp.addEventListener('click', () => {
-    const contentPkp = document.querySelector('.pkp')
-    contentPkp.classList.add('move-up')
-})
