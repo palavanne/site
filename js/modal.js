abe3dacb675187d53291e54d433e6908f0fee7c2
veuda <62720180+veuda@users.noreply.github.com>
2020-07-26 01:34:13 +0200

    maj optimisation

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,24 @@
 const teaser = document.querySelector('.teaser-img')
-teaser.addEventListener('click', function () {
-    const modalOpen = document.querySelector('.modal')
-    modalOpen.classList.remove('hidden')
+const modal = document.querySelector('.modal')
+const btnClose = document.querySelector('.btn-close')
 
-    const teaserDiv = document.querySelector('#teaser')
+teaser.addEventListener('click', function () {
+    modal.classList.remove('hidden')
+
+    const teaser = document.querySelector('#teaser')
     const videoSrc = document.createElement('iframe')
     videoSrc.classList.add('player', 'youtube-video')
     videoSrc.src = 'https://www.youtube.com/embed/29pGVEAHnzs?enablejsapi=1&version=3&playerapiid=ytplayer'
     videoSrc.setAttribute('frameborder', 0)
     videoSrc.setAttribute('allowfullscreen', true)
-    teaserDiv.appendChild(videoSrc)
+    teaser.appendChild(videoSrc)
     setTimeout(() => {
         document.querySelector('.youtube-video').contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
     }, 1000);
 })
 
-const btnClose = document.querySelector('.btn-close')
 btnClose.addEventListener('click', function () {
     const iframe = document.querySelector('.player')
-    const modal = document.querySelector('.modal')
     modal.classList.add('hidden')
     iframe.remove()
 })
